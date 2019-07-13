0d218db90129fab9c168fbd8fd260772b38edb5c
lishuai2016 <1830473670@qq.com>
2019-07-14 00:02:17 +0800

    本地源码阅读环境调通

---
+++ eureka-client/src/main/java/com/netflix/ls/Lishuai.java
@@ -0,0 +1,12 @@
+package com.netflix.ls;
+
+/**
+ * @program: eureka
+ * @author: lishuai
+ * @create: 2019-07-14 00:00
+ */
+public class Lishuai {
+    public static void main(String[] args) {
+        System.out.println("666666");
+    }
+}
